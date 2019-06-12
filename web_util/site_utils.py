329598a47ab3e5a54c52f817784295d8fdedadb3
iggy12345 <aaronjencks@gmail.com>
2019-06-11 23:23:23 -0500

    Finished email part

--- web_util/site_utils.py
+++ web_util/site_utils.py
@@ -2,3 +2,8 @@
 def remove_dollar_and_comma(data: str) -> int:
     """Removes the $ and , from the labels on the display for the price sliders."""
     return int(data.replace('$', '').replace(',', ''))
+
+
+def insert_dollar_sign_and_commas(data: int) -> str:
+    """Takes a string of numbers and inserts the '$' and ','"""
+    return '${:,}'.format(data)
--- web_util/source_util.py
+++ web_util/source_util.py
@@ -1,4 +1,5 @@
 from display_util.string_display_util import print_notification
+from email_util import EmailServer
 
 from selenium import webdriver
 
@@ -58,10 +59,65 @@ class Source:
         if auto_start:
             self.find_new_listings()
 
+    def __del__(self):
+        if self.browser is not None:
+            self.browser.close()
+
     def on_new_listing(self, car: Car):
-        # TODO: Have email me
         print_notification("Found car: {}".format(car))
         pass
 
     def find_new_listings(self):
         pass
+
+
+class EmailingSource(Source):
+    """Same as a normal source, except that 'on_new_listing' emails the target email address."""
+
+    def __init__(self, target_email: str, searches: list, browser=None, smtp_server: EmailServer = None, auto_start: bool = True):
+        super().__init__(searches, browser, False)
+
+        if smtp_server is None:
+            smtp_server = EmailServer()
+
+        self.email_server = smtp_server
+        self.email_callback = target_email
+
+        if auto_start:
+            self.start()
+
+    def __del__(self):
+        super().__del__()
+        if self.email_server.is_running:
+            self.email_server.stop()
+
+    def start(self):
+        self.email_server.start()
+        self.find_new_listings()
+
+    def create_email(self, car: Car) -> str:
+        """Creates an email string composed of the car's information"""
+        return """
+        <html>
+          <body>
+            <p><b>[AUTOMATED EMAIL, DO NOT REPLY]</b><br>
+            <br>
+            A car of the requirements that you specified has just been listed<br>
+            <a href={}>{}</a><br>
+            <br>
+            Best,<br>
+            <br>
+            Jarvis</p>
+          </body>
+        </html>
+        """.format(car.url, str(car))
+
+    def on_new_listing(self, car: Car):
+        super().on_new_listing(car)
+
+        email = self.create_email(car)
+
+        self.email_server.send_mail(self.email_callback, str(car), email)
+
+
+
